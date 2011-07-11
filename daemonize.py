da2fb39116d598cb47e84f25e3b396c46527d100
Quentin Stievenart <quentin.stievenart@gmail.com>
2011-07-11 23:34:48 +0200

    adding daemonize script

---
+++ daemonize.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from grizzled.os import daemonize
+from os import execv, getcwd, chdir
+
+PYTHON_PATH = '/usr/local/bin/'
+PYTHON2_EXECUTABLE = 'python'
+
+# daemonize sets the cwd to /, so keep a track of the current cwd...
+cwd = getcwd()
+daemonize(no_close=True, pidfile=cwd+'/paste.pid')
+# ... and we go back in this directory
+chdir(cwd)
+execv(PYTHON_PATH + PYTHON2_EXECUTABLE, [PYTHON2_EXECUTABLE, 'tpaste.py'])
+
