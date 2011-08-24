5f6823ca1345fd7bfa298cdada75357d9508902b
Arthur Carcano (NougatRillettes) <arthur.carcano@orange.fr>
2011-08-25 01:54:54 +0200

    Rajout de l'user, avec user par défault, et d'une détection de langage.

--- paste.sh
+++ paste.sh
@@ -1,19 +1,31 @@
 #!/bin/sh
 URL=http://paste.awesom.eu
+DEFAULTUSER=Babass
+PREFEREDLANG=default
 
 if test -n "$1" ; then
     FILE=$1
 else
-    echo "Usage: $0 file [lang]"
+    echo "Usage: $0 file [lang] [user]"
     exit
 fi
 
 if test -n "$2" ; then
     LANG=$2
 else
-    LANG=
+    case `echo $1| awk -F . '{ print ($(NF))}'` in
+	    hs)		LANG=haskell;;
+    	    py)		LANG=python;;
+	    *)		LANG=$PREFEREDLANG;;
+    esac
 fi
 
-PASTE=$(curl -d "hl=${LANG}&escape=on&script" --data-urlencode paste@${FILE} $URL 2>/dev/null)
+if test -n "$3" ; then
+	USER=$3
+else
+	USER=$DEFAULTUSER
+fi
+
+PASTE=$(curl -d "hl=${LANG}&user=${USER}&escape=on&script" --data-urlencode paste@${FILE} $URL 2>/dev/null)
 
 echo "$URL/$PASTE"
